a7e4b13b01c25ac98e8efd7d7feb566de2ff6c94
J.Ferec <janferec@gmail.com>
2026-05-10 23:32:43 +0200

    add color definitions

--- board.py
+++ board.py
@@ -1,6 +1,11 @@
-from enum import Enum
+from enum import IntEnum
 
-
-class Color(Enum):
+class Color (IntEnum):
     WHITE = 0
     BLACK = 1
+
+    def __invert__(self):
+        if self == self.WHITE:
+            return self.BLACK
+        else:
+            return self.WHITE
